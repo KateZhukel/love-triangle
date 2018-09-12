7b861f7411d929d9e75c63d27798871b7d1d64ab
Kate Zhukel <zhukelkatya@gmail.com>
2018-09-12 10:08:51 +0300

    love triangle solution

--- src/index.js
+++ src/index.js
@@ -3,5 +3,24 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    let pair = [];
+    let count = 0;
+    for (let i = 0; i <= preferences.length -1 ; i++) {
+        let firstLoved = preferences[i];
+        let secondLoved = preferences[firstLoved-1];
+        let thirdLoved = preferences[secondLoved-1];
+        if(firstLoved === secondLoved || secondLoved === thirdLoved ||
+            thirdLoved === firstLoved){
+            continue;
+        }
+        if(thirdLoved === i+1 && (!pair.includes(firstLoved) ||
+                                !pair.includes(secondLoved) ||
+                                !pair.includes(thirdLoved))){
+            pair.push(firstLoved);
+            pair.push(secondLoved);
+            pair.push(thirdLoved);
+            count++;
+        }
+    }
+    return count;
 };
